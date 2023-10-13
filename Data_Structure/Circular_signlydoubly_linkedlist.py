08eb8f29dbe0bffa65c59e6f92f074fb2b2659da
Manjunath C <cmanjunath128@gmail.com>
2023-10-13 22:40:30 +0530

    Update Circular_signlydoubly_linkedlist.py

--- Data_Structure/Circular_signlydoubly_linkedlist.py
+++ Data_Structure/Circular_signlydoubly_linkedlist.py
@@ -1,16 +1,36 @@
+class Circular_Signly_linked_list:
+    def __init__(self,data):
+        self.data=data
+        self.next_node_addr=None
+    def set_data(self,new):
+        self.data=new
+    def get_data(self):
+        return self.data
+    def set_next_addr(self,node_addr):
+        self.next_node_addr=node_addr
+    def get_next_addr(self):
+        return self.next_node_addr   
+        
+node1=Signly_linked_list(10)
+node2=Signly_linked_list(20)
+node3=Signly_linked_list(30)
+node4=Signly_linked_list(40)
+
+node1.set_next_addr(node2)  
+node2.set_next_addr(node3) 
+node3.set_next_addr(node4)
+# last node stores next addr as first node address
+node4.set_next_addr(node1)
+
+print(node1.get_next_addr())
+print(node2.get_next_addr())
+print(node3.get_next_addr())
 
 
-
-
-
-
-
-
-
-
-
-
-
+print(node1.get_data())
+print(node2.get_data())
+print(node3.get_data())
+print(node4.get_data())
 
 
 class Circular_Doubly_linked_list:
