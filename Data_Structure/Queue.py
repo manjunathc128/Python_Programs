1c87dbafb331433771ff3fc37dbfa2d918a121d5
Manjunath C <cmanjunath128@gmail.com>
2023-10-14 07:49:36 +0530

    Create Queue.py

---
+++ Data_Structure/Queue.py
@@ -0,0 +1,34 @@
+"""
+
+Queue: Queue is temprovary memory used to store the data in linear and  First in first out passion .
+Queue works on the principle of FIFO, LILO.
+the process of adding elements to queue is called as Enqueue operation.
+the process of removing elements from queue is called as Dequeue operation.
+
+"""
+
+class Queue:
+    def __init__(self,size):
+        self.l=[]
+        self.size=size
+    def Enqueue(self,data):
+        if len(self.l) == self.size:
+            raise ValueError('Queue is full')
+        self.l.append(data)
+        
+    def Dequeue(self,index):
+        if len(self.l) == 0:
+            raise ValueError('Queue is Empty')
+        self.l.pop(index)
+        
+    def showQueue(self):
+        return self.l
+Queue1=Queue(5)  
+
+Queue1.Enqueue(1)
+Queue1.Enqueue(2)
+Queue1.Enqueue(3)
+Queue1.Enqueue(4)
+Queue1.Enqueue(5)
+
+print(Queue1.showQueue())
