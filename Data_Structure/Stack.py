c5dce9829f54a23f5b1f66af4f16c8ad92407d43
Manjunath C <cmanjunath128@gmail.com>
2023-10-14 07:17:48 +0530

    Update Stack.py

--- Data_Structure/Stack.py
+++ Data_Structure/Stack.py
@@ -1,7 +1,17 @@
+'''
+Stack: Stack is temprorary memory that stores the data in the linear and first in last out passsion 
+stack works on the principle of FILO, LIFO
+Stack has only one end elements can be accessed only in one direction.
+
+''' 
+
+
 class Stack:
   def __init__(self,size):
     self.l=[]
     self.size=size
+  def show(self):
+      return self.l
   def push(self,data):
     if len(self.l) == self.size:
       raise ValueError('stack is full')
@@ -12,4 +22,11 @@ class Stack:
       raise ValueError('stack is empty')
     else:
       self.l.pop()
-s1=Stack(5)      
+      
+s1=Stack(5)   
+s1.push(1)
+s1.push(2)
+s1.push(3)
+s1.push(4)
+s1.push(5)
+print(s1.show())
