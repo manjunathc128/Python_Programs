3f38dffaf06e21e38e7bea4f400332581645daef
Manjunath C <cmanjunath128@gmail.com>
2023-10-20 00:02:37 +0530

    Update factorial.py

--- Interview_problems/factorial.py
+++ Interview_problems/factorial.py
@@ -6,4 +6,14 @@ def factorial(n):
     return n * factorial(n-1)
 print(factorial(5)) 
 
-# find the factorail of all the number upto given number
+# find the factorail of all the number upto given number using recursion
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n-1)
+    
+num = int(input("enter the number: " ))   
+
+for val in range(2,num+1):
+    print(factorial(val))
